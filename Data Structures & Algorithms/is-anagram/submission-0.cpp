888e75741461fc159583ff6f9c76ef36b7c2d592
Sumedh Tribhuvan <sumedh.tribhuvan24@vit.edu>
2026-07-22 12:39:13 +0530

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+      unordered_map<char, int>mp;
+
+      for(char c:s){
+        mp[c]++;
+      } 
+      for(char c : t){
+        mp[c]--;
+      }
+      for(auto it: mp){
+        if(it.second != 0)
+        return false;
+      }
+      return true;
+        
+        
+    }
+};
